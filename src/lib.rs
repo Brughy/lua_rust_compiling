4226ccc5cb44462be3a6a8154d8d8ccdb81e14b8
Luca Brugnera <lbrugnera@jmawireless.com>
2022-08-02 16:18:33 +0200

    Add iterator into source dir

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,38 @@
+use std::fs;
+use std::path::Path;
+
 extern crate gcc;
 
 #[warn(dead_code)]
 fn main() {
-    gcc::Build::new()
-        .file("../lua/src/lapi.c")
-        .file("../lua/src/lauxlib.c")
-        .file("../lua/src/lbaselib.c")
-        .file("../lua/src/lcode.c")
-        .file("../lua/src/lcorolib.c")
-        .file("../lua/src/lctype.c")
-        .file("../lua/src/ldblib.c")
-        .file("../lua/src/ldebug.c")
-        .file("../lua/src/ldo.c")
-        .file("../lua/src/ldump.c")
-        .file("../lua/src/lfunc.c")
-        .file("../lua/src/lgc.c")
-        .file("../lua/src/linit.c")
-        .file("../lua/src/liolib.c")
-        .file("../lua/src/llex.c")
-        .file("../lua/src/lmathlib.c")
-        .file("../lua/src/lmem.c")
-        .file("../lua/src/loadlib.c")
-        .file("../lua/src/lobject.c")
-        .file("../lua/src/lopcodes.c")
-        .file("../lua/src/loslib.c")
-        .file("../lua/src/lparser.c")
-        .file("../lua/src/lstate.c")
-        .file("../lua/src/lstring.c")
-        .file("../lua/src/lstrlib.c")
-        .file("../lua/src/ltable.c")
-        .file("../lua/src/ltablib.c")
-        .file("../lua/src/ltm.c")
-        .file("../lua/src/lua.c")
-        .file("../lua/src/luac.c")
-        .file("../lua/src/lundump.c")
-        .file("../lua/src/lutf8lib.c")
-        .file("../lua/src/lvm.c")
-        .file("../lua/src/lzio.c")
-        .include("../lua/src")
-        .flag("-O2")
-        .define("LUA_USE_APICHECK", Some("1"))
-        .compile("liblua.a");
+	
+	//let lib_include_path = Path::new("/");
+	let include_path = Path::new("lua/src/");
+	let path = fs::read_dir(include_path).unwrap();
+	
+    let mut cfg = gcc::Build::new();    
+
+	cfg.flag("-O2").define("LUA_USE_APICHECK", Some("1"));
+	//cfg.flag_if_supported();
+	//cfg.include(lib_path)
+	//.define("FOO","BAR")
+	  
+    for ifile in path {
+		    match ifile {
+				Ok(p) => {  let y = p.path().display().to_string();
+							if (y.contains(".c")) {
+								cfg.file(<String as AsRef<Path>>::as_ref(&y));
+						    }
+						  },
+				Err(e) => {println!("Error in PathBuf");}
+			} 
+	}
+    
+    cfg.include(include_path);
+
+      //cfg.target("arm-linux-gnueabihf");
+        cfg.compile("liblua.a");
+      //.shared_flag(true)
+      //.static_flag(true)
+      //.compile("liblua.so")
 }
